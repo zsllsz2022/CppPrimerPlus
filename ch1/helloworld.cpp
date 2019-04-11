a25ac2d901f09c5f36a2558739d685824060841e
David Zhan <david@kf-robotics.com>
2019-04-11 18:00:24 +0800

    ch1

---
+++ ch1/helloworld.cpp
@@ -0,0 +1,20 @@
+
+//编译
+// $ g++ helloworld.cpp
+
+//运行
+// $ ./a.out
+
+
+// $ g++ helloworld.cpp -o hello
+// $ ./hello 
+
+
+
+#include <iostream>
+
+int main(int argc,char** argv){
+
+	std::cout << "hello world" << std::endl;
+	return 0;
+}
